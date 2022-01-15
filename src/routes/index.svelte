aa6eec22d73e33ad75248b9acf04f771a5cac0fb
Maria Anson <mariaansona@gmail.com>
2022-01-15 10:09:09 +0530

    Add first component; Add Tailwind CSS and daisyUI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  import { client } from '$lib/graphql-client'
+  import { gql } from 'graphql-request'
+
+  export const load = async () => {
+    const query = gql`
+      query GetProjects {
+        projects {
+          name
+          slug
+          description
+          tags
+          demo
+          sourceCode
+          image {
+            url
+          }
+        }
+      }
+    `
+    const { projects } = await client.request(query)
+
+    return {
+      props: {
+        projects,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let projects
+</script>
+
+<h1>Recent Projects by Me</h1>
+
+<div>
+  {#each projects as { name, slug, description, image }}
+    <div>
+      <img src={image[0].url} alt={name} />
+      <a href={`/projects/${slug}`}>
+        <div>
+          <h2>{name}</h2>
+          <p>
+            {description.slice(0, 80)}...
+          </p>
+        </div>
+      </a>
+    </div>
+  {/each}
+</div>
