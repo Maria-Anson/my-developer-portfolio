5decf1049b1692eb9fee6bee90f4df0e36ff5e3a
Maria Anson <mariaansona@gmail.com>
2022-01-15 12:26:33 +0530

    Add theme select to navbar

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -27,6 +27,22 @@ const PROJECT_FRAGMENT = gql`
     }
   }
 `
+const POST_FRAGMENT = gql`
+  fragment PostDetails on Post {
+    title
+    slug
+    date
+    content
+    tags
+    coverImage {
+      url
+    }
+    authors {
+      name
+    }
+  }
+`
+
 
 export const projectsQuery = gql`
   ${PROJECT_FRAGMENT}
@@ -42,6 +58,24 @@ export const projectQuery = gql`
   query GetProject($slug: String!) {
     project(where: { slug: $slug }) {
       ...ProjectDetails
+    }
+  }
+`
+
+export const postsQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPosts {
+    posts {
+      ...PostDetails
+    }
+  }
+`
+
+export const postQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      ...PostDetails
     }
   }
 `
