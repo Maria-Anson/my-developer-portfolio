ddf57a02b297e49c488c75db3e4724e7d4db3413
Maria Anson <mariaansona@gmail.com>
2022-02-27 20:55:30 +0530

    Adding markdown option to render them

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -33,7 +33,7 @@
   <div class="flex mb-40 items-end">
     <div class="mr-6">
       <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
-      <p class="text-xl mb-4">{intro}</p>
+      <p class="text-xl mb-4">{@html marked(intro)}</p>
     </div>
   
     <img class="mask mask-squircle h-48" src={url} alt={name} />
