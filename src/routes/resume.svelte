2f5bb436b7b95b61f6081ec8deaf1b7756f57bef
Maria Anson <mariaansona@gmail.com>
2022-03-26 12:27:27 +0530

    Fixed mobile iframe scaling issue

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,35 +1,30 @@
 
-<html>
-  
-<head>
-    <style>
-        body {
-            background-color: LIGHTGREY;
-        }
-          
-        #target {
-            width: 800px;
-            height: 1000px;
-            overflow-y: auto;
-            overflow-x: auto;
-            resize: both;
-            position: relative;
-            z-index: 2;
-        }
-          
-        iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
-  
-</head>
+<article div class="prose prose-lg">
+<style>
+    body {
+        background-color: LIGHTGREY;
+    }
+        
+    #target {
+        width: auto;
+        height: 1070px;
+        overflow-y: auto;
+        overflow-x: auto;
+        resize: both;
+        position: relative;
+        z-index: 2;
+    }
+        
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+    }
+</style>
   
 <body>
     <div id="target">
-        <iframe scrolling="no" src="https://docs.google.com/document/d/e/2PACX-1vQr4o2fxqaLxDV5UfMyjBxDVssho2f4RY7FF_tIyfZujBfyE2P5Jb031fXHjIENMg/pub?embedded=true">></iframe>
+        <iframe title="Resume" scrolling="no" src="https://docs.google.com/document/d/e/2PACX-1vQRywyM_8EwhvOOnUFGjafsnQgmNRv8PguZtqVpTzlQWCnMjTrlHR3AwJrYHqftKA/pub?embedded=true"></iframe>
     </div>
 </body>
-  
-</html>
+</article>
