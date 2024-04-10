57a12093b504b0b4fe39292dcdc7a896cdc5fabd
Maria Anson <mariaansona@gmail.com>
2024-04-10 10:09:49 -0400

    Update resume.svelte

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -25,8 +25,9 @@
 <body>
     <div id="target">
         <!-- <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQRywyM_8EwhvOOnUFGjafsnQgmNRv8PguZtqVpTzlQWCnMjTrlHR3AwJrYHqftKA/pub?embedded=true"></iframe> -->
-        <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQObjQPbyuCzynKEn2nfGJWoR2ge5wsotl9UQ_MGdi2Q2Qny-EHsc3rXwMUCtqMhA/pub?embedded=true"></iframe>
+        <!-- <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQObjQPbyuCzynKEn2nfGJWoR2ge5wsotl9UQ_MGdi2Q2Qny-EHsc3rXwMUCtqMhA/pub?embedded=true"></iframe> -->
         <!-- <iframe title="Resume" scrolling="yes" src="https://drive.google.com/file/d/1rqSCKguPiKcLfo7Q9CZJE8ZCRZWMI3qk/view?usp=sharing" width="100%" height="500"></iframe> -->
+        <iframe src="https://drive.google.com/file/d/1frbh4FnZxMn3d1KM-dF5EkQ-ic1p6QDz/preview" width="640" height="480" allow="autoplay"></iframe>
     </div>
 </body>
 </article>
