240a226b4a8a5acf96a3a287ec183ffb7b89af6d
Maria Anson <mariaansona@gmail.com>
2024-04-10 09:54:45 -0400

    Update resume.svelte

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -24,7 +24,8 @@
   
 <body>
     <div id="target">
-        <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQRywyM_8EwhvOOnUFGjafsnQgmNRv8PguZtqVpTzlQWCnMjTrlHR3AwJrYHqftKA/pub?embedded=true"></iframe>
+        <!-- <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQRywyM_8EwhvOOnUFGjafsnQgmNRv8PguZtqVpTzlQWCnMjTrlHR3AwJrYHqftKA/pub?embedded=true"></iframe> -->
+        <iframe title="Resume" scrolling="yes" src="https://docs.google.com/document/d/e/2PACX-1vQObjQPbyuCzynKEn2nfGJWoR2ge5wsotl9UQ_MGdi2Q2Qny-EHsc3rXwMUCtqMhA/pub?embedded=true"></iframe>
     </div>
 </body>
 </article>
